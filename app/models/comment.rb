77d4fe036c8f437355543f7452f5a994bf902cf1
romasks <lusersks@gmail.com>
2016-06-05 10:05:40 +0200

    Issue3 & Issue1_1 - Add validation & modify view of posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :post, presence: true
   validates :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   has_many :comments
   validates :title, :body, presence: true
+  validates :title, length: {
+    minimum: 10,
+    maximum: 100,
+    too_short: "title must have at least %{count} characters",
+    too_long: "title must have at most %{count} characters"
+  }
 end
