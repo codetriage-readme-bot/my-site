f4d5d39d5a724269c8430d84dcf935f08b1fb48c
romasks <lusersks@gmail.com>
2016-06-17 00:04:39 +0200

    Magic with seeds.rb

--- Gemfile
+++ Gemfile
@@ -15,21 +15,19 @@ gem 'sdoc', '~> 0.4.0', group: :doc  # bundle exec rake doc:rails generates the 
 # gem 'unicorn'                 # Use Unicorn as the app server
 # gem 'capistrano-rails', group: :development     # Use Capistrano for deployment
 
-group :test do
-  gem 'byebug'                      # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'pg'
+
+group :test, :development do
+  gem 'byebug'                  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
 end
 
 group :development do
-  gem 'web-console', '~> 2.0'       # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'byebug'
-  gem 'pg'
+  gem 'web-console', '~> 2.0'   # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'seed_dumper'
 end
 
-group :production do
-  gem 'rails_12factor'
-  gem 'pg'
-end
+gem 'rails_12factor', group: :production
+gem 'pg'
+
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,28 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 
-Post.create(title: 'Blog App', body: 'Application with using SQLite, AngularJS & Bootstrap')
-Post.create(title: 'Rails or PHP', body: 'What would you choose - Rails 4.2 or PHP 5.4 ?')
-Post.create(title: 'Full Stack Dev', body: 'How quickly you can become developers over all web-stack?
+Post.create(:id => 1, :title => 'Blog App', body => 'Application with using SQLite, AngularJS & Bootstrap')
+Post.create(id: 2, title: 'Rails or PHP', body: 'What would you choose - Rails 4.2 or PHP 5.4 ?')
 
+Post.create(id: 3, title: 'Full Stack Dev', body: 'How quickly you can become developers over all web-stack?
 The barrier of entering the web development industry as a web developer is still low, but it’s getting increasingly complex.
-
-The dynamic nature of the whole industry makes requirements shift often to the most popular and “next best thing” tools and programming languages.
-
-Gone are the days when only one programming language or a very specific process was required from a developer. Nowadays programmers must know a range of technologies across multiple platforms in order to do good work.
+The dynamic nature of the whole industry makes requirements shift often to the most popular and “next best thing” tools
+and programming languages.
+Gone are the days when only one programming language or a very specific process was required from a developer.
+Nowadays programmers must know a range of technologies across multiple platforms in order to do good work.
 What does a full-stack developer mean?
-
 The term full-stack means developers who are comfortable working with both back-end and front-end technologies.
+To be more specific, it means that the developer can work with databases, PHP, HTML, CSS,
+JavaScript and everything in between, also, venturing as far as converting Photoshop designs to front-end code.
+A full-stack developer doesn’t need to master all of the areas and technologies he needs to work it, because that just makes
+it nearly impossible, he just needs to be comfortable working with those technologies, and that’s a lot too.
+What full-stack meant in 2000 and what it means now?
+...
+to be continue...
+...')
 
-To be more specific, it means that the developer can work with databases, PHP, HTML, CSS, JavaScript and everything in between, also, venturing as far as converting Photoshop designs to front-end code.
-
-A full-stack developer doesn’t need to master all of the areas and technologies he needs to work it, because that just makes it nearly impossible, he just needs to be comfortable working with those technologies, and that’s a lot too.
-What full-stack meant in 2000 and what it means now?')
-Post.create(title: 'New post', body: 'This is a new fancy post!')
-Post.create(title: 'The post number 5', body: 'BODY')
+Post.create(id: 4, title: 'New post', body: 'This is a new fancy post!')
+Post.create(id: 5, title: 'The post number 5', body: 'BODY')
 
 Comment.create(post_id: 1, body: 'Hello, people!')
 Comment.create(post_id: 2, body: 'The First comment for post #2')
