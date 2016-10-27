d30c50506c9664266b8db98b5b37368b7ab2bbf9
Raman Skaskevich <lusersks@gmail.com>
2016-10-27 07:49:33 +0200

    Some RSpec test and some fix

--- spec/request/about_spec.rb
+++ spec/request/about_spec.rb
@@ -1,9 +1,32 @@
 describe "About Page" do
   describe "GET /about" do
     
-    it "request success" do
+    before (:each) do
       visit "/about"
+    end
+    
+    it "request success" do
       expect(page.status_code).to be(200)
+    end
+    
+    describe "musthead" do
+      
+      it "has title" do
+        expect(page).to have_selector 'h2', text: "About Me"
+      end
+      
+      it "has name" do
+        expect(page).to have_selector 'strong', text: "Raman Skaskevich"
+      end
+      
+      it "has link to wiki"
+      
+      it "has link to Ericpol page" do
+        #expect(page).to click_link('Ericsson Ericpol').redirect_to('http://www.ericpol.com/')
+      end
+      
+      it "has link to projects page"
+      
     end
     
   end
--- spec/request/projects_spec.rb
+++ spec/request/projects_spec.rb
@@ -1,9 +1,32 @@
 describe "Projects Page" do
   describe "GET /projects" do
     
-    it "request success" do
+    before(:each) do
       visit "/projects"
+    end
+    
+    it "request success" do
       expect(page.status_code).to be(200)
+    end
+    
+    describe "musthead" do
+      
+      it "has title" do
+        expect(page).to have_selector 'h2', text: "Projects"
+      end
+      
+      it "has project romasks" do
+        expect(page).to have_selector 'strong', text: "romasks"
+      end
+      
+      it "has project oldtree" do
+        expect(page).to have_selector 'strong', text: "oldtree"
+      end
+      
+      it "has project hairdo-mysql" do
+        expect(page).to have_selector 'strong', text: "hairdo-mysql"
+      end
+      
     end
     
   end
