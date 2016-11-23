2f60f749d9ff8db0453b4856c5e1198662b414c7
Raman Skaskevich <lusersks@gmail.com>
2016-11-23 20:32:18 +0100

    Add partial as Admin Area

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,12 @@ class DashboardController < ApplicationController
     #@time = Time.now
     #@files = Dir.glob('*')
   end
+  
+  def about
+    p @soc_net = params[:soc_net]
+    p @item = params[:item]
+    
+    #curl -H "Authorization: Bearer <token>" "https://api.linkedin.com/v1/people/~:raman-skaskevich-919b92bb?format=json"
+    #https://api.linkedin.com/v1/people-search:(people:(raman-skaskevich-919b92bb))?first-name=parameter&last-name=parameter
+  end
 end
