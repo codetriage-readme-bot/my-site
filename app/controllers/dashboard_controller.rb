ca53ed38885167648e57cb4cc14d627c49f54311
Raman Skaskevich <lusersks@gmail.com>
2016-11-29 00:20:22 +0100

    Refactor of JSON parser

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,98 +5,67 @@ class DashboardController < ApplicationController
   end
   
   def about
-    p @soc_net = params[:soc_net]
-    p @item = params[:item]
+    @soc_net = params[:soc_net]
+    @item = params[:item]
     
-    #curl -H "Authorization: Bearer <token>" "https://api.linkedin.com/v1/people/~:raman-skaskevich-919b92bb?format=json"
-    #https://api.linkedin.com/v1/people-search:(people:(raman-skaskevich-919b92bb))?first-name=parameter&last-name=parameter
-    
-    getListOfItems()
-  end
-  
-  def getListOfItems()
-    # 1 - Determine social network
     if @soc_net == 'LinkedIn'
 
-      # 2 - Parse corresponding file
       jsonFile = File.read("/home/roman/workspace/romasks/app/controllers/resume.json")
-      jsonData = JSON.parse(jsonFile)
+      @jsonData = JSON.parse(jsonFile)
       
-      # 3 - Search needed items
+      @arr = Array.new
+      @item = "Work" if @item == "Experience"
+      @item = @item.downcase
       
-      # Summary
-      if @item == "Summary"
-        hash = jsonData["basics"]
-        str = hash["summary"]
-        arr = Array.new
-        arr << str
-        @items = arr
-      end
+      send("parse_#{@item}")
       
-      # Skills
-      if @item == "Skills"
-        hash = jsonData["skills"]
-        
-        arr = Array.new
-        hash.each { |item|
-          arr << item['name']
-        }
-        
-        @items = arr
-      end
-      
-      # Experience
-      if @item == "Experience"
-        hash = jsonData["work"]
+      @items = @arr
+    end
     
-        arr = Array.new
-        hash.each { |item|
-          date_end = (item['endDate'] || 'current')
-          str = "#{item['company']}, #{item['position']} \n (#{item['startDate']} - #{date_end})"
-          arr << str
-        }
-        
-        @items = arr
-      end
-      
-      # Education
-      if @item == "Education"
-        hash = jsonData["education"]
-    
-        arr = Array.new
-        hash.each { |item|
-          if item['studyType'] != ""
-            type = ", #{item['studyType']}"
-          else
-            type = ""
-          end
-          
-          str = "#{item['institution']}#{type} (#{item['startDate']} - #{item['endDate']})"
-          arr << str
-        }
-        
-        @items = arr
-      end
-      
-      # Languages
-      if @item == "Languages"
-        hash = jsonData["languages"]
-    
-        arr = Array.new
-        hash.each { |item|
-          str = "#{item['language']} - #{item['fluency']}"
-          arr << str
-        }
-        
-        @items = arr
-      end
-      
-      # Courses
-      if @item == "Courses"
-        arr = Array.new
-        @items = arr
-      end
-      
-    end    
   end
+  
+  def parse_summary
+    # Summary
+    @arr << @jsonData["basics"]["summary"]
+  end
+  
+  def parse_skills
+    # Skills
+    @jsonData["skills"].each { |item|
+      @arr << item['name']
+    }
+  end
+  
+  def parse_education
+    # Education
+    @jsonData["education"].each { |item|
+      if item['studyType'] != ""
+        type = ", #{item['studyType']}"
+      else
+        type = ""
+      end
+      @arr << "#{item['institution']}#{type} (#{item['startDate']} - #{item['endDate']})"
+    }
+  end
+  
+  def parse_work
+    # Experience
+    @jsonData["work"].each { |item|
+      date_end = (item['endDate'] || 'current')
+      @arr << "#{item['company']}, #{item['position']} (#{item['startDate']} - #{date_end})"
+    }
+  end
+  
+  def parse_languages
+    # Languages
+    @jsonData["languages"].each { |item|
+      @arr << "#{item['language']} - #{item['fluency']}"
+    }
+  end
+  
+  def parse_courses
+    # Courses
+    # nothing there
+  end
+  
 end
