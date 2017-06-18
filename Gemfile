73bb0a9e50ad30362319f81eeae415d36dccebad
Raman Skaskevich <lusersks@gmail.com>
2017-06-18 08:49:07 +0200

    Add Redcarpet gem for markdown Post body

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'haml', '~> 5.0', '>= 5.0.1'
 gem 'simple_form', '~> 3.5'
-
+gem 'redcarpet'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
 feature 'create_new_post' do
-	let(:btn_new) { btn_new = 'Create New One' }
-	let(:btn_create) { btn_new = 'Create Post' }
-
 	scenario 'create new post with valid data' do
 		visit '/posts'
-		click_on(btn_new)
+		click_on('Create New One')
 
 		fill_in('Title', with: 'Make a Post')
 		fill_in('Body', with: 'Post content')
-		click_on(btn_create)
+		click_on('Create Post')
 
 		within(".flash-container") do
 			expect(page).to have_content 'Post was successfully created.'
@@ -20,9 +17,9 @@ feature 'create_new_post' do
 
 	scenario 'create new post with invalid data' do
 		visit '/posts'
-		click_on(btn_new)
+		click_on('Create New One')
 
-		click_on(btn_create)
+		click_on('Create Post')
 
 		within(".errors-container") do
 			msg_expect = "2 errors prevented this Post from saving"
