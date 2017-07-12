d5b8374dfbde6aaa9240ce8888c6dec79f45a644
lusersks <lusersks@gmail.com>
2017-07-12 07:08:38 -0400

    Modify Sentry configuration

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Raven.configure do |config|
-  config.dsn = 'https://32fc602c59484c87bf2b3ca89bb20b5d:3a43ab4f70e145b697bd55cfded8e4c7@sentry.io/190106'
-end
-
 module Romasks
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
